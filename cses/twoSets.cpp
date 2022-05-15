4fc564720382de94ce6ee3aa1da48219bedc9ffd
abidmuin <muin.739@gmail.com>
2022-05-15 10:34:36 +0600

    Two Sets

---
+++ cses/twoSets.cpp
@@ -0,0 +1,52 @@
+// https://cses.fi/problemset/task/1092
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll int n = 0, sum = 0, mid = 0;
+	cin >> n;
+	sum = (n * (n + 1)) / 2;
+
+	if (sum % 2 == 0)
+	{
+		cout << "YES" << endl;
+		mid = sum / 2;
+		vector<int> v1, v2;
+		vector<int>::iterator it;
+		for (ll int i = n; i > 0; i--)
+		{
+			if (i <= mid)
+			{
+				v1.push_back(i);
+				mid -= i;
+			}
+			else
+			{
+				v2.push_back(i);
+			}
+		}
+		cout << v1.size() << endl;
+		for (it = v1.begin(); it != v1.end(); it++)
+		{
+			cout << *it << " ";
+		}
+		cout << endl;
+		cout << v2.size() << endl;
+		for (it = v2.begin(); it != v2.end(); it++)
+		{
+			cout << *it << " ";
+		}
+		cout << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+
+	return 0;
+}
