3eb33668696df7c0a2d48319d12e45db47346668
abidmuin <muin.739@gmail.com>
2022-08-27 22:21:04 +0600

    Find the Town Judge

---
+++ leetcode/findTheTownJudge.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        vector<pair<int,int> > v(n+1,{0,0});
+        
+        for(int i=0; i<trust.size(); i++){
+            v[trust[i][0]].first++;
+            v[trust[i][1]].second++;
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(v[i].first==0 && v[i].second==(n-1)){
+                return i;
+            }
+        }
+        
+        
+        return -1;
+    }
+};
