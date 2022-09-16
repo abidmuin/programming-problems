22bd9ef22d9f9c46ee08789418fe298e6d2f81c8
Abid Hasan Muin <muin.739@gmail.com>
2022-09-16 17:39:06 +0600

    Watermelon

---
+++ Codeforces/watermelon.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/4/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int weight = 0;
+	cin >> weight;
+
+	if (weight > 3 && (weight & 1) == 0)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+	return 0;
+}
