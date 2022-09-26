24c8a9e234b89288bb79c60488f0a3f3f56094d4
Abid Hasan Muin <muin.739@gmail.com>
2022-09-26 18:11:43 +0600

    Reverse String

--- LeetCode/reverse-string.cpp
+++ LeetCode/reverse-string.cpp
@@ -12,20 +12,19 @@ public:
 
 	void reverseString(vector<char> &s)
 	{
-		int stringLength = s.size();
 		int leftPointer = 0;
-		int rightPointer = stringLength - 1;
-		int midPoint = stringLength / 2;
+		int rightPointer = s.size() - 1;
 
-		while (leftPointer < midPoint)
+		while (leftPointer < rightPointer)
 		{
 			swapCharacter(s[leftPointer], s[rightPointer]);
 			leftPointer++;
 			rightPointer--;
 		}
-        
-        for(auto itr=s.begin(); itr!=s.end(); itr++){
-            cout << *itr << "";
-        }
+
+		for (auto itr = s.begin(); itr != s.end(); itr++)
+		{
+			cout << *itr << "";
+		}
 	}
 };
