2e49a68c2a44e9b88f2a725e24befb6e96f6912b
abidmuin <muin.739@gmail.com>
2023-01-04 19:22:32 +0600

    Word Capitalization

---
+++ Codeforces/word-capitalization.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/281/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+
+	if (islower(str[0]))
+	{
+		str[0] = toupper(str[0]);
+	}
+
+	cout << str << endl;
+
+	return 0;
+}
