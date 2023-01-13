2c27208bfb94845165fa75b60a9a482fb9d3953d
abidmuin <muin.739@gmail.com>
2023-01-13 23:25:30 +0600

    Replace Elements w/ Greatest Element on Right Side

--- LeetCode/replace-elements-with-greatest-element-on-right-side.cpp
+++ LeetCode/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
 
+// Brute Force
+
 class Solution
 {
 public:
@@ -22,6 +24,36 @@ public:
 				arr[i] = -1;
 			}
 		}
+
+		return arr;
+	}
+};
+
+/*
+Optimal Solution
+
+Time Complexity => O(N)
+Space Complexity => O(1) 
+*/
+class Solution
+{
+public:
+	vector<int> replaceElements(vector<int> &arr)
+	{
+		int vecSize = arr.size();
+		int currentMax = arr[vecSize - 1];
+		int temp = 0;
+
+		for (int i = (vecSize - 2); i >= 0; i--)
+		{
+			temp = currentMax;
+			if (arr[i] > currentMax)
+			{
+				currentMax = arr[i];
+			}
+			arr[i] = temp;
+		}
+		arr[vecSize - 1] = -1;
 
 		return arr;
 	}
