7ae7c616ad5216e97abb5edcee32e0216b091727
abidmuin <muin.739@gmail.com>
2023-01-15 18:08:54 +0600

    Length of Last Word

--- LeetCode/length-of-last-word.cpp
+++ LeetCode/length-of-last-word.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/length-of-last-word/
 
+// Iterate Left to Right
+
 class Solution
 {
 public:
@@ -23,6 +25,39 @@ public:
 				vecSize = v.size();
 				currentMax = max(currentMax, vecSize);
 				v.clear();
+			}
+		}
+
+		return currentMax;
+	}
+};
+
+// Iterate Right to Left
+
+class Solution
+{
+public:
+	int lengthOfLastWord(string s)
+	{
+		vector<char> v;
+		int strLength = s.size();
+		int currentMax = 0;
+
+		for (int i = strLength - 1; i >= 0; i--)
+		{
+			if (s[i] != ' ')
+			{
+				currentMax = 0;
+				v.push_back(s[i]);
+			}
+
+			if ((s[i] == ' ') || (i == 0))
+			{
+				currentMax = v.size();
+				if (currentMax > 0)
+				{
+					break;
+				}
 			}
 		}
 
