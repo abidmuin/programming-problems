f00431aea8fb001137b3f6aafd739664a0cadce0
abidmuin <muin.739@gmail.com>
2023-01-23 10:53:26 +0600

    Find the Town Judge

--- LeetCode/find-the-town-judge.cpp
+++ LeetCode/find-the-town-judge.cpp
@@ -5,10 +5,11 @@ class Solution
 public:
 	int findJudge(int n, vector<vector<int>> &trust)
 	{
+		int vecSize = trust.size();
 
 		vector<pair<int, int>> v(n + 1, {0, 0});
 
-		for (int i = 0; i < trust.size(); i++)
+		for (int i = 0; i < vecSize; i++)
 		{
 			v[trust[i][0]].first++;
 			v[trust[i][1]].second++;
