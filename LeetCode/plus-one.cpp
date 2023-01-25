10eadd53289bdad02bade999d2750437b33056c1
abidmuin <muin.739@gmail.com>
2023-01-25 11:08:29 +0600

    Plus One

--- LeetCode/plus-one.cpp
+++ LeetCode/plus-one.cpp
@@ -2,24 +2,24 @@
 
 class Solution
 {
-    public:
-        vector<int> plusOne(vector<int> &digits)
-        {
-            int arraySize = digits.size();
-            int result = 0;
+public:
+	vector<int> plusOne(vector<int> &digits)
+	{
+		int arraySize = digits.size();
+		int result = 0;
 
-            for (int i = arraySize - 1; i >= 0; i--)
-            {
-                if (digits[i] < 9)
-                {
-                    digits[i]++;
-                    return digits;
-                }
-                digits[i]=0;
-            }
-            
-            digits.insert(digits.begin(), 1);
-            
-            return digits;
-        }
+		for (int i = arraySize - 1; i >= 0; i--)
+		{
+			if (digits[i] < 9)
+			{
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
+
+		digits.insert(digits.begin(), 1);
+
+		return digits;
+	}
 };
