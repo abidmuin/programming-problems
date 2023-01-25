02edd08f3f94efd1e04a437b87280bec8dcdb132
abidmuin <muin.739@gmail.com>
2023-01-25 11:08:50 +0600

    Missing Number

--- LeetCode/missing-number.cpp
+++ LeetCode/missing-number.cpp
@@ -1,15 +1,18 @@
 // https://leetcode.com/problems/missing-number/
 
-class Solution {
+class Solution
+{
 public:
-    int missingNumber(vector<int>& nums) {
-        int missingNumber = 0;
-        int len = nums.size();
-        
-        for(int i=0; i<len; i++){
-            missingNumber = missingNumber ^ nums[i] ^ (i+1);
-        }
-        
-        return missingNumber;
-    }
+	int missingNumber(vector<int> &nums)
+	{
+		int missingNumber = 0;
+		int len = nums.size();
+
+		for (int i = 0; i < len; i++)
+		{
+			missingNumber = missingNumber ^ nums[i] ^ (i + 1);
+		}
+
+		return missingNumber;
+	}
 };
