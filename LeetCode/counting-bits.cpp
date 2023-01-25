d38c6aa0adac4ad43793428477fc7d580e60f18a
abidmuin <muin.739@gmail.com>
2023-01-25 11:09:12 +0600

    Counting Bits

--- LeetCode/counting-bits.cpp
+++ LeetCode/counting-bits.cpp
@@ -2,21 +2,25 @@
 
 // Brian Kernighan's Algorithm - https://iq.opengenus.org/brian-kernighan-algorithm/
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> countBits(int n) {
-        vector<int> v;
-        int bitCount = 0;
-        int currentNum = 0;
-        for(int i=0; i<=n; i++){
-            currentNum = i;
-            while(currentNum){
-                currentNum = currentNum & (currentNum-1);
-                bitCount++;
-            }
-            v.push_back(bitCount);
-            bitCount = 0;
-        }
-        return v;
-    }
+	vector<int> countBits(int n)
+	{
+		vector<int> v;
+		int bitCount = 0;
+		int currentNum = 0;
+		for (int i = 0; i <= n; i++)
+		{
+			currentNum = i;
+			while (currentNum)
+			{
+				currentNum = currentNum & (currentNum - 1);
+				bitCount++;
+			}
+			v.push_back(bitCount);
+			bitCount = 0;
+		}
+		return v;
+	}
 };
