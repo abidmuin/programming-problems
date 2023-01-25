09386fb369df249b1681aa09e27c512f5bd8bd19
abidmuin <muin.739@gmail.com>
2023-01-25 11:10:33 +0600

    Longest Uncommon Subsequence I

--- LeetCode/longest-uncommon-subsequence-i.cpp
+++ LeetCode/longest-uncommon-subsequence-i.cpp
@@ -1,13 +1,17 @@
 // https://leetcode.com/problems/longest-uncommon-subsequence-i/
 
-class Solution {
+class Solution
+{
 public:
-    int findLUSlength(string a, string b) {
-        if(a!=b){
-            return a.size() > b.size() ? a.size() : b.size();
-        }
-        else{
-            return -1;
-        }
-    }
+	int findLUSlength(string a, string b)
+	{
+		if (a != b)
+		{
+			return a.size() > b.size() ? a.size() : b.size();
+		}
+		else
+		{
+			return -1;
+		}
+	}
 };
