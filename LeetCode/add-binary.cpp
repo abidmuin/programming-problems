fbde11b6ac3b1ce4dba38c17949d73f9682bb2a8
abidmuin <muin.739@gmail.com>
2023-02-14 19:28:18 +0600

    Add Binary

--- LeetCode/add-binary.cpp
+++ LeetCode/add-binary.cpp
@@ -1,79 +1,50 @@
 // https://leetcode.com/problems/add-binary/
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution
 {
 public:
 	string addBinary(string a, string b)
 	{
 		string result = "";
+		int aSize = a.size() - 1;
+		int bSize = b.size() - 1;
+		int binarySum = 0;
 		int carry = 0;
-		int stringSize = max(a.size(), b.size());
-		string::iterator itr;
-		itr = result.begin();
 
-		for (int i = stringSize - 1; i >= 0; i--)
+		while (aSize >= 0 || bSize >= 0)
 		{
-			if ((a[i] == 1) && (b[i] == 1))
+			binarySum = carry;
+
+			if (aSize >= 0)
 			{
-				if (carry == 1)
-				{
-					result.insert(itr, '1');
-					carry = 1;
-				}
-				else
-				{
-					result.insert(itr, '0');
-					carry = 1;
-				}
-				cout << result << endl;
+				binarySum += a[aSize] - '0';
 			}
-			else if ((a[i] == 0) && (b[i] == 0))
+			if (bSize >= 0)
 			{
-				if (carry == 1)
-				{
-					result.insert(itr, '1');
-					carry = 0;
-				}
-				cout << result << endl;
+				binarySum += b[bSize] - '0';
+			}
+
+			result += to_string(binarySum % 2);
+			aSize--;
+			bSize--;
+
+			if (binarySum > 1)
+			{
+				carry = 1;
 			}
 			else
 			{
-				if (carry == 1)
-				{
-					result.insert(itr, '0');
-					carry = 1;
-				}
-				else
-				{
-					result.insert(itr, '1');
-					carry = 0;
-				}
-				cout << result << endl;
+				carry = 0;
 			}
 		}
 
-		if (carry == 1)
+		if (carry)
 		{
-			result.insert(itr, '1');
-			carry = 0;
+			result += to_string(carry);
 		}
+
+		reverse(result.begin(), result.end());
 
 		return result;
 	}
 };
-
-int main()
-{
-	Solution sol;
-
-	string x = "1010";
-	string y = "1011";
-
-	cout << sol.addBinary(x, y) << endl;
-
-	return 0;
-}
