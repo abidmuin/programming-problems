844e5e6312283f2fa37dcb74af99a60362788dbe
Muin <muin.739@gmail.com>
2023-02-28 19:43:34 +0600

    Registration system

---
+++ Codeforces/registration-system.cpp
@@ -0,0 +1,38 @@
+// https://codeforces.com/problemset/problem/4/C
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int numberOfRequests = 0;
+	cin >> numberOfRequests;
+
+	string name;
+	map<string, int> m; // string=name, int=suffix number
+	map<string, int>::iterator itr;
+
+	while (numberOfRequests--)
+	{
+		cin >> name;
+		itr = m.find(name);
+
+		if (itr != m.end())
+		{
+			name += to_string(++m[name]);
+			cout << name << endl;
+		}
+		else
+		{
+			cout << "OK" << endl;
+			m.insert({name, 0});
+		}
+	}
+
+	return 0;
+}
