499a0cb7fd09be2f42a2c21cae82f9daca7b233c
Muin <muin.739@gmail.com>
2023-03-02 20:42:10 +0600

    v1

---
+++ Codeforces/police-recruits.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/427/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int numberOfEvents = 0;
+	cin >> numberOfEvents;
+
+	int recruits = 0;
+	int crimes = 0;
+	int untreatedCrimes = 0;
+
+	return 0;
+}
