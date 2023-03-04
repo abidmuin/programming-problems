ac889fc5dc8ef57e1fbd99a2df3936dc304a5f66
abidmuin <muin.739@gmail.com>
2023-03-04 23:32:31 +0600

    implemented prime check function

--- Codeforces/t-primes.cpp
+++ Codeforces/t-primes.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 bool isPrime(long long int num)
 {
+	if (num <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i <= sqrt(num); i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
 int main()
