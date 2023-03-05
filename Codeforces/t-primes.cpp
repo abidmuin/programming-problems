fe83e4711c7910aeaf416f267e89aa840716047f
Muin <muin.739@gmail.com>
2023-03-05 10:56:42 +0600

    T-primes

--- Codeforces/t-primes.cpp
+++ Codeforces/t-primes.cpp
@@ -6,14 +6,24 @@ using namespace std;
 
 bool isPrime(long long int num)
 {
-	if (num <= 1)
+	if (num < 2)
 	{
 		return false;
 	}
 
-	for (int i = 2; i <= sqrt(num); i++)
+	if (num < 4)
 	{
-		if (num % i == 0)
+		return true;
+	}
+
+	if (num % 2 == 0 || num % 3 == 0)
+	{
+		return false;
+	}
+
+	for (int i = 5; i * i <= num; i = i + 6)
+	{
+		if (num % i == 0 || num % (i + 2) == 0)
 		{
 			return false;
 		}
@@ -24,22 +34,18 @@ bool isPrime(long long int num)
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	int n = 0;
-	cin >> n;
+	long long int testCases = 0;
+	cin >> testCases;
 
 	long long int num = 0;
 	long long int sqrtOfNum = 0;
 
-	while (n--)
+	while (testCases--)
 	{
 		cin >> num;
 		sqrtOfNum = sqrt(num);
 
-		if (isPrime(num) && (sqrtOfNum * sqrtOfNum == n))
+		if (isPrime(sqrtOfNum) && (sqrtOfNum * sqrtOfNum == num))
 		{
 			cout << "YES";
 		}
