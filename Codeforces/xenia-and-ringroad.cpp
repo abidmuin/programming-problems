3b214570d99bb18c707fc37c2123beed87d74a28
Muin <muin.739@gmail.com>
2023-03-05 18:50:48 +0600

    Xenia and Ringroad

---
+++ Codeforces/xenia-and-ringroad.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/339/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	return 0;
+}
