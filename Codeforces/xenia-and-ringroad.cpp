aa2ad57f4da612a5acbd6caa30b0eb8cd5ba7bdf
abidmuin <muin.739@gmail.com>
2023-03-05 20:15:26 +0600

    Xenia and Ringroad

--- Codeforces/xenia-and-ringroad.cpp
+++ Codeforces/xenia-and-ringroad.cpp
@@ -10,5 +10,32 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	int houses = 0;
+	int tasks = 0;
+
+	cin >> houses >> tasks;
+
+	long long int time = 0;
+	int currentTask = 0;
+	int currentPosition = 1;
+
+	for (int i = 0; i < tasks; i++)
+	{
+		cin >> currentTask;
+
+		if (currentTask >= currentPosition)
+		{
+			time += (currentTask - currentPosition);
+			currentPosition = currentTask;
+		}
+		else
+		{
+			time += (houses - currentPosition) + currentTask;
+			currentPosition = currentTask;
+		}
+	}
+
+	cout << time << endl;
+
 	return 0;
 }
