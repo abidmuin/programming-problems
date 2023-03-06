2a642b13e26aa32a3ef90c9b995ffe62aeb20368
abidmuin <muin.739@gmail.com>
2023-03-06 23:29:05 +0600

    v1

---
+++ Codeforces/sum-of-round-numbers.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/1352/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int testCases = 0;
+	cin >> testCases;
+
+	string number;
+	int minOperations = 0;
+	int remainder = 0;
+
+	while (testCases--)
+	{
+		cin >> number;
+
+		
+
+	}
+
+	return 0;
+}
