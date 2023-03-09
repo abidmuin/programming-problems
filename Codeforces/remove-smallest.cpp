1525b3f01731a74f580ff6d7722f9c99f0b6b69d
Muin <muin.739@gmail.com>
2023-03-09 14:35:55 +0600

    v1

---
+++ Codeforces/remove-smallest.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/1399/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	
+
+	return 0;
+}
