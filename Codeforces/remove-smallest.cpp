82fa8fad2326f5188376ec070fb688090f40421c
abidmuin <muin.739@gmail.com>
2023-03-09 19:58:53 +0600

    Remove Smallest

--- Codeforces/remove-smallest.cpp
+++ Codeforces/remove-smallest.cpp
@@ -10,7 +10,39 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	
+	int testCases = 0;
+	cin >> testCases;
+
+	int vecSize = 0;
+	while (testCases--)
+	{
+		cin >> vecSize;
+		vector<int> v(vecSize);
+
+		for (int i = 0; i < vecSize; i++)
+		{
+			cin >> v[i];
+		}
+
+		sort(v.begin(), v.end());
+
+		for (int i = 0; i < vecSize; i++)
+		{
+			if (((v[i + 1] - v[i]) > 1) &&
+				((i + 1) < v.size()))
+			{
+				cout << "NO";
+				break;
+			}
+			else if ((i + 1) == v.size())
+			{
+				cout << "YES";
+				break;
+			}
+		}
+
+		cout << "\n";
+	}
 
 	return 0;
 }
