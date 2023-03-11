567edcfb24f27c953e064b0994b89d4214ff1196
abidmuin <muin.739@gmail.com>
2023-03-12 00:26:46 +0600

    Design Tutorial: Learn from Math

---
+++ Codeforces/design-tutorial-learn-from-math.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/472/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int num = 0;
+	cin >> num;
+
+	if (num & 1)
+	{
+		cout << (num - 9) << " " << 9 << endl;
+	}
+	else
+	{
+		cout << (num - 8) << " " << 8 << endl;
+	}
+
+	return 0;
+}
