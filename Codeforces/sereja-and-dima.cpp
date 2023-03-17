bde7907d3fb6a59cd39d633235e57b13c2a11a8d
abidmuin <muin.739@gmail.com>
2023-03-17 23:12:47 +0600

    Sereja and Dima

---
+++ Codeforces/sereja-and-dima.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/381/A
+
+/*
+Time Complexity =>
+Space Complexity =>
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	return 0;
+}
