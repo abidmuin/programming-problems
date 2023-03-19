7c0b50462b1efdd337f0a40150bffb29e198cb95
Muin <muin.739@gmail.com>
2023-03-19 18:35:07 +0600

    Sereja and Dima

--- Codeforces/sereja-and-dima.cpp
+++ Codeforces/sereja-and-dima.cpp
@@ -1,13 +1,5 @@
 // https://codeforces.com/problemset/problem/381/A
 
-<<<<<<< HEAD
-=======
-/*
-Time Complexity =>
-Space Complexity =>
-*/
-
->>>>>>> 598a00a74ef04a5fdd97ba9f2a15e76f370fb61e
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -18,7 +10,6 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-<<<<<<< HEAD
 	int cards = 0;
 	cin >> cards;
 
@@ -69,7 +60,5 @@ int main()
 
 	cout << sereja << " " << dima << endl;
 
-=======
->>>>>>> 598a00a74ef04a5fdd97ba9f2a15e76f370fb61e
 	return 0;
 }
