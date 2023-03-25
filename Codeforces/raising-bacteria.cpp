ef87a335d2f935b5b2e927f0baa2d2cfcece3da2
abidmuin <muin.739@gmail.com>
2023-03-25 23:30:29 +0600

    v1

---
+++ Codeforces/raising-bacteria.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/579/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	return 0;
+}
