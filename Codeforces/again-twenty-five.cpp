41cdc3e3df07e27080ca103d219ad1c41df29dac
abidmuin <muin.739@gmail.com>
2023-03-27 20:28:35 +0600

    Again Twenty Five!

---
+++ Codeforces/again-twenty-five.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/630/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n = 0;
+	cin >> n;
+
+	cout << 25 << endl;
+
+	return 0;
+}
