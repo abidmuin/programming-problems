eb43a5dc5f46d7870b132f64aa17054ab290f649
Muin <muin.739@gmail.com>
2023-03-27 08:45:13 +0600

    Dislike of Threes

--- Codeforces/dislike-of-threes.cpp
+++ Codeforces/dislike-of-threes.cpp
@@ -13,7 +13,32 @@ int main()
 	int testCases = 0;
 	cin >> testCases;
 
-	
+	int k = 0;
+	int sequenceCounter = 0;
+
+	while (testCases--)
+	{
+		cin >> k;
+
+		for (int i = 1;; i++)
+		{
+			if ((i % 3 == 0) || (i % 10 == 3))
+			{
+				continue;
+			}
+			else
+			{
+				sequenceCounter++;
+			}
+
+			if (sequenceCounter == k)
+			{
+				cout << i << "\n";
+				sequenceCounter = 0;
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
