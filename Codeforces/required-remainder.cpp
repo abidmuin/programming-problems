32be899b2ab5b8a5fb72ee6f97039fa84e8f545a
abidmuin <muin.739@gmail.com>
2023-04-07 02:52:21 +0600

    Required Remainder

--- Codeforces/required-remainder.cpp
+++ Codeforces/required-remainder.cpp
@@ -10,5 +10,21 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	int testCases = 0;
+	cin >> testCases;
+
+	long long int x = 0, y = 0, n = 0;
+	long long int p = 0; // arbitary value
+	long long int k = 0; // result
+
+	while (testCases--)
+	{
+		cin >> x >> y >> n;
+		p = floor((n - y) / x);
+		k = (p * x) + y;
+
+		cout << k << endl;
+	}
+
 	return 0;
 }
