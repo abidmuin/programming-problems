2c274d38ce045f4545b167dcf4b243adc8f6061b
abidmuin <muin.739@gmail.com>
2023-04-07 23:23:53 +0600

    v1

---
+++ Codeforces/even-array.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/1367/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	return 0;
+}
