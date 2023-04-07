6f0f0cd0acaff211f88d91f3b4682c3289e84c83
abidmuin <muin.739@gmail.com>
2023-04-07 23:51:22 +0600

    Even Array

--- Codeforces/even-array.cpp
+++ Codeforces/even-array.cpp
@@ -10,5 +10,55 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	int testCases;
+	cin >> testCases;
+
+	int arrayLength = 0;
+	int oddElementsInOddIndex = 0;
+	int oddElementsCounter = 0;
+	int totalOddIndices = 0;
+	int temp = 0;
+	vector<int> v;
+
+	while (testCases--)
+	{
+		cin >> arrayLength;
+
+		for (int i = 0; i < arrayLength; i++)
+		{
+			cin >> temp;
+			v.push_back(temp);
+
+			if (v[i] % 2 != 0)
+			{
+				oddElementsCounter++;
+
+				if (i % 2 != 0)
+				{
+					oddElementsInOddIndex++;
+				}
+			}
+		}
+
+		totalOddIndices = (arrayLength / 2);
+		// cout << totalOddIndices << endl;
+
+		if (oddElementsCounter != totalOddIndices)
+		{
+			cout << -1 << endl;
+		}
+		else
+		{
+			cout << (oddElementsCounter - oddElementsInOddIndex) << endl;
+		}
+
+		// cout << oddElementsCounter << endl;
+		// cout << oddElementsInOddIndex << endl;
+
+		oddElementsCounter = 0;
+		oddElementsInOddIndex = 0;
+		v.clear();
+	}
+
 	return 0;
 }
