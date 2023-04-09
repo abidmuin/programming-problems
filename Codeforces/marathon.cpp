2b9cc4731a5130c11ed3db9efb7d50e7f77038b1
Abid Hasan Muin <muin.739@gmail.com>
2023-04-09 23:15:21 +0600

    v2

--- Codeforces/marathon.cpp
+++ Codeforces/marathon.cpp
@@ -9,6 +9,15 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
+	
+	int testCases=0;
+	cin >> testCases;
+	
+	int a=0, b=0, c=0, d=0;
+	
+	while(testCases--){
+	cin >> a >> b >> c >> d;
+	}
 
 	return 0;
 }
