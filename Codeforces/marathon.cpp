82a667378f50310cf62da774eaac7a801324a459
abidmuin <muin.739@gmail.com>
2023-04-09 23:46:12 +0600

    Marathon

--- Codeforces/marathon.cpp
+++ Codeforces/marathon.cpp
@@ -9,14 +9,32 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-	
-	int testCases=0;
+
+	int testCases = 0;
 	cin >> testCases;
-	
-	int a=0, b=0, c=0, d=0;
-	
-	while(testCases--){
-	cin >> a >> b >> c >> d;
+
+	int a = 0, b = 0, c = 0, d = 0;
+	int result = 0;
+
+	while (testCases--)
+	{
+		cin >> a >> b >> c >> d;
+
+		if (a < b)
+		{
+			result++;
+		}
+		if (a < c)
+		{
+			result++;
+		}
+		if (a < d)
+		{
+			result++;
+		}
+
+		cout << result << "\n";
+		result = 0;
 	}
 
 	return 0;
