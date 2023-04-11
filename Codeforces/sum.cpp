4c3cf802ee7ab344392cbe90220f7cf92e04e51c
abidmuin <muin.739@gmail.com>
2023-04-11 23:33:39 +0600

    v1

---
+++ Codeforces/sum.cpp
@@ -0,0 +1,34 @@
+// https://codeforces.com/problemset/problem/1742/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int testCases = 0;
+	cin >> testCases;
+
+	int a = 0, b = 0, c = 0;
+
+	while (testCases--)
+	{
+		cin >> a >> b >> c;
+
+		if ((a == b) && (b == c))
+		{
+			cout << "NO"
+				 << "\n";
+		}
+		else
+		{
+			
+		}
+	}
+
+	return 0;
+}
