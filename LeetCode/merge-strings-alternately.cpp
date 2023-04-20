78f59321f221cd98dcd190f6eb097aea04c5356d
abidmuin <muin.739@gmail.com>
2023-04-20 23:30:17 +0600

    Merge Strings Alternately

--- LeetCode/merge-strings-alternately.cpp
+++ LeetCode/merge-strings-alternately.cpp
@@ -5,5 +5,24 @@ class Solution
 public:
 	string mergeAlternately(string word1, string word2)
 	{
+		string result = "";
+
+		int word1Length = word1.size();
+		int word2Length = word2.size();
+		int strLength = max(word1Length, word2Length);
+
+		for (int i = 0; i < strLength; i++)
+		{
+			if (i < word1Length)
+			{
+				result += word1[i];
+			}
+			if (i < word2Length)
+			{
+				result += word2[i];
+			}
+		}
+
+		return result;
 	}
 };
