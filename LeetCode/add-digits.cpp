1d8fd1516d081e5fe1d30ab59abaf23ff34e9b43
abidmuin <muin.739@gmail.com>
2023-04-26 11:25:51 +0600

    Add Digits

--- LeetCode/add-digits.cpp
+++ LeetCode/add-digits.cpp
@@ -46,3 +46,27 @@ public:
 		}
 	}
 };
+
+// ITERATION
+class Solution
+{
+public:
+	int addDigits(int num)
+	{
+		int tempSum = 0;
+
+		while (num > 9)
+		{
+			while (num)
+			{
+				tempSum += (num % 10);
+				num /= 10;
+			}
+
+			num = tempSum;
+			tempSum = 0;
+		}
+
+		return num;
+	}
+};
