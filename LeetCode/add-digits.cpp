b33cb2c100772bcf71846f154f3723cbc2e01fae
abidmuin <muin.739@gmail.com>
2023-04-26 11:21:31 +0600

    added Recursion approach

--- LeetCode/add-digits.cpp
+++ LeetCode/add-digits.cpp
@@ -21,3 +21,28 @@ public:
 		}
 	}
 };
+
+// RECURSION
+class Solution
+{
+public:
+	int addDigits(int num)
+	{
+		int result = 0;
+
+		while (num)
+		{
+			result += (num % 10);
+			num /= 10;
+		}
+
+		if (result < 10)
+		{
+			return result;
+		}
+		else
+		{
+			return addDigits(result);
+		}
+	}
+};
