4921453df4ac64f6b20532abb26e185117d47bda
abidmuin <muin.739@gmail.com>
2023-05-27 20:02:29 +0600

    Sleep

---
+++ LeetCode/sleep.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/sleep/
+
+/**
+ * @param {number} millis
+ */
+
+async function sleep(millis) {
+	return new Promise((resolve, reject) => {
+		if (typeof millis !== "number" || isNaN(millis)) {
+			reject(new Error('Invalid argument. Expected a number.'));
+		} else {
+			setTimeout(resolve, millis);
+		}
+	});
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
