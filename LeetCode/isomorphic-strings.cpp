d8d37af91dc450a92bfd888964c2a13645ffba22
abidmuin <muin.739@gmail.com>
2023-06-21 14:48:31 +0600

    Isomorphic Strings

--- LeetCode/isomorphic-strings.cpp
+++ LeetCode/isomorphic-strings.cpp
@@ -1,49 +1,25 @@
 // https://leetcode.com/problems/isomorphic-strings/
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution
 {
 public:
 	bool isIsomorphic(string s, string t)
 	{
 		int stringSize = s.size();
-		map<char, int> m1;
-		map<char, int> m2;
+		int s_map[128] = {0};
+		int t_map[128] = {0};
 
 		for (int i = 0; i < stringSize; i++)
 		{
-			if (m1[s[i]] && (m1[s[i]] != t[i]))
+			if (s_map[s[i]] != t_map[t[i]])
 			{
 				return false;
 			}
 
-			if (m2[t[i]] && (m2[t[i]] != s[i]))
-			{
-				return false;
-			}
-
-			m1[s[i]] = t[i];
-			m2[t[i]] = s[i];
+			s_map[s[i]] = i + 1;
+			t_map[t[i]] = i + 1;
 		}
 
 		return true;
 	}
 };
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	string s = "egg";
-	string t = "add";
-
-	Solution sol;
-	cout << sol.isIsomorphic(s, t) << endl;
-
-	return 0;
-}
