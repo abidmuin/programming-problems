d36492f38581104f04d889999bb8659686181d70
abidmuin <muin.739@gmail.com>
2023-06-23 10:15:15 +0600

    Final Value of Variable After Performing Operations

--- LeetCode/final-value-of-variable-after-performing-operations.cpp
+++ LeetCode/final-value-of-variable-after-performing-operations.cpp
@@ -1,9 +1,5 @@
 // https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution
 {
 public:
@@ -13,37 +9,16 @@ public:
 
 		for (auto &&element : operations)
 		{
-			for (auto &&ch : element)
+			if (element[1] == '+')
 			{
-				if (ch == '+')
-				{
-					value++;
-					break;
-				}
-				else if (ch == '-')
-				{
-					value--;
-					break;
-				}
+				value++;
+			}
+			else if (element[1] == '-')
+			{
+				value--;
 			}
 		}
 
 		return value;
 	}
 };
-
-using namespace std;
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	vector<string> operations = {"++X", "++X", "X++"};
-
-	Solution sol;
-	cout << sol.finalValueAfterOperations(operations) << endl;
-
-	return 0;
-}
