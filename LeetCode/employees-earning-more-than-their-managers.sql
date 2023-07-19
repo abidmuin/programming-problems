fed2afe1370211c12103720a2a4f4fb83d519696
abidmuin <muin.739@gmail.com>
2023-07-19 18:48:04 +0600

    Employees Earning More Than Their Managers

---
+++ LeetCode/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,5 @@
+/* https://leetcode.com/problems/employees-earning-more-than-their-managers/ */
+SELECT e.name AS Employee
+FROM Employee e
+	JOIN Employee m ON e.managerId = m.id
+WHERE e.salary > m.salary;
