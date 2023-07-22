138692aad5ba03ff10ac9378f2e3b1e15d85d81d
abidmuin <muin.739@gmail.com>
2023-07-22 07:55:26 +0600

    Rising Temperature

---
+++ LeetCode/rising-temperature.sql
@@ -0,0 +1,5 @@
+/* https://leetcode.com/problems/rising-temperature/ */
+SELECT w1.id
+FROM Weather w1
+	JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
