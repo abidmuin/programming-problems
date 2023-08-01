c9dfc59d8329966817972f57ba2b83df64f8cc32
abidmuin <muin.739@gmail.com>
2023-08-01 10:53:24 +0600

    Black Square

--- Codeforces/black-square.cpp
+++ Codeforces/black-square.cpp
@@ -4,16 +4,40 @@
 
 using namespace std;
 
+int calculateCalories(vector<int> &a, string &s)
+{
+	int totalCalories = 0;
+	int stripNumber = 0;
+
+	for (char i : s)
+	{
+		stripNumber = i - '0' - 1;
+		totalCalories += a[stripNumber];
+	}
+
+	cout << endl;
+
+	return totalCalories;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int a[4] = {0};
-	cin >> a[0] >> a[1] >> a[2] >> a[3];
-	
-	int result = 0;	
+	int numberOfStrips = 4;
+	vector<int> a(numberOfStrips);
+	string s;
+
+	for (int i = 0; i < numberOfStrips; i++)
+	{
+		cin >> a[i];
+	}
+
+	cin >> s;
+
+	cout << calculateCalories(a, s) << endl;
 
 	return 0;
 }
