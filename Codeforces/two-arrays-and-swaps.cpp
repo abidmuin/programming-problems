19fe9508c4c99e80b0a48765f35784a2e4c6f3ee
abidmuin <muin.739@gmail.com>
2023-09-07 03:19:56 +0600

    Split a String in Balanced Strings

---
+++ Codeforces/two-arrays-and-swaps.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/1353/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int testCases = 0;
+	cin >> testCases;
+
+	
+
+	return 0;
+}
