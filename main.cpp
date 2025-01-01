31d88f2643cb6693aaad57245d0cc958763c64b0
abidmuin <muin.739@gmail.com>
2025-01-01 16:05:01 +0600

    cpp workflow template

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+
+/*
+Time Complexity =>
+Space Complexity =>
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
