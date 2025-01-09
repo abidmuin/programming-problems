fdd0fd23eda56d80f832c2951cd0663102c5ef61
abidmuin <muin.739@gmail.com>
2025-01-09 20:08:53 +0600

    Simple Sum

---
+++ beecrowd/1003.cpp
@@ -0,0 +1,28 @@
+// https://judge.beecrowd.com/en/problems/view/1003
+
+/*
+Time Complexity =>
+Space Complexity =>
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.14159;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  ifstream input("input.txt");
+  ofstream output("output.txt");
+
+  int a = 0, b = 0;
+  cin >> a >> b;
+
+  cout << "SOMA = " << (a + b) << endl;
+
+  return 0;
+}
