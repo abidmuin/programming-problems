b96bf6c36291da787c689f05b4c90363fcc67798
abidmuin <muin.739@gmail.com>
2025-01-09 18:40:11 +0600

    Extremely Basic

---
+++ beecrowd/1001.cpp
@@ -0,0 +1,27 @@
+// https://judge.beecrowd.com/en/problems/view/1001
+
+/*
+Time Complexity =>
+Space Complexity =>
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  ifstream input("input.txt");
+  ofstream output("output.txt");
+
+  int a = 0, b = 0;
+  cin >> a >> b;
+
+  int x = a + b;
+  cout << "X = " << x << endl;
+
+  return 0;
+}
