07e8e8b49bfe6b01d35bef83407914f3952c4967
abidmuin <muin.739@gmail.com>
2025-01-11 23:02:21 +0600

    Twins

--- Codeforces/twins.cpp
+++ Codeforces/twins.cpp
@@ -4,38 +4,32 @@
 
 using namespace std;
 
-int main()
-{
-	int coins = 0;
-	cin >> coins;
+int main() {
+  int coins = 0;
+  cin >> coins;
 
-	vector<int> v;
-	int temp = 0;
-	int totalSum = 0;
+  vector<int> v(coins);
+  int totalSum = 0;
 
-	while (coins--)
-	{
-		cin >> temp;
-		totalSum += temp;
-		v.push_back(temp);
-	}
+  for (int i = 0; i < coins; i++) {
+    cin >> v[i];
+    totalSum += v[i];
+  }
 
-	sort(v.begin(), v.end(), greater<int>{});
+  sort(v.begin(), v.end(), greater<int>{});
 
-	int vecSize = v.size();
-	int myCoinsValue = v[0];
-	int minCoins = 1;
+  int myCoinsValue = 0;
+  int minCoins = 1;
 
-	for (int i = 1; i < vecSize; i++)
-	{
-		if ((totalSum - myCoinsValue) >= myCoinsValue)
-		{
-			myCoinsValue += v[i];
-			minCoins++;
-		}
-	}
+  for (int i = 0; i < coins; i++) {
+    myCoinsValue += v[i];
+    minCoins++;
+    if ((2 * myCoinsValue) > totalSum) {
+      break;
+    }
+  }
 
-	cout << minCoins << endl;
+  cout << minCoins << endl;
 
-	return 0;
+  return 0;
 }
