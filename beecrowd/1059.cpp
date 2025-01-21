06d51a894a7748f50811c6fd85ef3382a0e614f3
abidmuin <muin.739@gmail.com>
2025-01-21 20:21:01 +0600

    Even Numbers

---
+++ beecrowd/1059.cpp
@@ -0,0 +1,25 @@
+// https://judge.beecrowd.com/en/problems/view/1059
+
+/*
+Time Complexity =>
+Space Complexity =>
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  ifstream input("input.txt");
+  ofstream output("output.txt");
+
+  for (int i = 2; i <= 100; i += 2) {
+    cout << i << endl;
+  }
+
+  return 0;
+}
