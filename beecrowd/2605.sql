b719dcf1ee8b4f39688d7edcbde72ed5c546d112
abidmuin <muin.739@gmail.com>
2025-01-31 12:58:00 +0600

    Executive Representatives

---
+++ beecrowd/2605.sql
@@ -0,0 +1,7 @@
+-- https://judge.beecrowd.com/en/problems/view/2605
+
+SELECT p.name AS product_name,
+       pr.name AS provider_name
+FROM products p
+JOIN providers pr ON p.id_providers = pr.id
+WHERE p.id_categories = 6;
