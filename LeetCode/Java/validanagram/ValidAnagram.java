b0b866337bdb1785c2a604ae0d387ae837ccdcf1
abidmuin <muin.739@gmail.com>
2025-07-03 15:19:57 +0600

    Valid Anagram

---
+++ LeetCode/Java/validanagram/ValidAnagram.java
@@ -0,0 +1,36 @@
+package validanagram;
+
+public class ValidAnagram {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		boolean result = sol.isAnagram("anagram", "nagaram");
+		System.out.println(result);
+	}
+}
+
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+class Solution {
+	public boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		// s and t consist of lowercase English letters
+		int[] arr = new int[26];
+
+		for (int i = 0; i < s.length(); i++) {
+			arr[s.charAt(i) - 'a']++;
+			arr[t.charAt(i) - 'a']--;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
