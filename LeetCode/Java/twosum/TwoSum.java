5169e38c60e3497462cb54571f916558bed4d12a
abidmuin <muin.739@gmail.com>
2025-07-03 17:09:42 +0600

    Two Sum

--- LeetCode/Java/twosum/TwoSum.java
+++ LeetCode/Java/twosum/TwoSum.java
@@ -11,6 +11,7 @@ public class TwoSum {
 	}
 }
 
+// https://leetcode.com/problems/two-sum
 //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
