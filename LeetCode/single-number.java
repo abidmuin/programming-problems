0b5938155f6045729456ecbc1ab987da71bb2e30
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-07-20 15:27:52 -0300

    Single Number

---
+++ LeetCode/single-number.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    int result = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      result ^= nums[i];
+    }
+
+    return result;
+  }
+}
